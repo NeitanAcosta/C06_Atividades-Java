d9776038470d4f79d04e9271b1d44dc42123191e
Nathan Arruola <163927856+NeitanAcosta@users.noreply.github.com>
2026-02-10 14:34:55 -0300

    1° Exercicio Java - POO

---
+++ Projeto 1 C06/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x  = 10;
+        int y = 20;
+
+        System.out.println("O valor é: " + (x+y));
+        System.out.println("Hello World!");
+    }
+}
