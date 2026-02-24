06edb6051e6fba658b30c53f679d1a202812a7f4
Nathan Arruola <163927856+NeitanAcosta@users.noreply.github.com>
2026-02-24 14:47:00 -0300

    Adivinhação 2 ( while/for/if) - Java

---
+++ Exercicio 6/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        //entrada de dados
+        Scanner entrada = new Scanner(System.in);
+
+        Random rand = new Random();
+        int x = rand.nextInt(10) + 1;
+
+        System.out.println("Adivinhe o numero: ");
+        int n = entrada.nextInt();
+
+        //loop para o usuario
+        while(n != x)
+        {
+            if(n >= x)
+            {
+                System.out.println("O numero é menor...");
+            }
+            else
+            {
+                System.out.println("O numero é maior...");
+            }
+            n = entrada.nextInt();
+        }
+
+        System.out.println("Você acertou o numero!: " +n );
+
+        entrada.close();
+    }
+}
