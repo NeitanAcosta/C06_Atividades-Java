4b64e58dbf2ecedb648805a13c7d7b01587a6b45
Nathan Arruola <163927856+NeitanAcosta@users.noreply.github.com>
2026-02-24 14:30:29 -0300

    Adivinhação ( while/for) - Java

---
+++ Exercicio 5/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        //entrada de dados
+        Scanner entrada = new Scanner(System.in);
+
+        Random rand = new Random();
+        int x = rand.nextInt(10) + 1;
+
+        System.out.println("Adivinhe o numero: ");
+        int n = entrada.nextInt();
+
+        //loop para o usuario
+        while(n != x)
+        {
+            System.out.println("Errou! Tente novamente: ");
+            n = entrada.nextInt();
+        }
+
+        System.out.println("Você acertou o numero!: " +n );
+
+        entrada.close();
+    }
+}
