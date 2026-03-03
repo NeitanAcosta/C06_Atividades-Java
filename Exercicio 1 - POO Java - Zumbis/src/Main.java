8f55eb5749834572ca0fd2915e3a2aac5905b6c8
Nathan Arruola <163927856+NeitanAcosta@users.noreply.github.com>
2026-03-03 14:12:05 -0300

    Exercicio 1 - POO Java - Zumbis
    Exercicio 1 com instâncias

---
+++ Exercicio 1 - POO Java - Zumbis/src/Main.java
@@ -0,0 +1,14 @@
+public class Main  {
+
+    public void main(String[] args){
+        Zumbi z1 = new Zumbi();
+        Zumbi z2 = new Zumbi();
+
+        z1.nome = "Chris";
+        z2.nome =  "Pizzoni";
+
+        z1.gritar();
+        z2.gritar();
+    }
+
+}
---
+++ Exercicio 1 - POO Java - Zumbis/src/Zumbi.java
@@ -0,0 +1,11 @@
+public class Zumbi {
+
+  String nome;
+  float velocidade;
+
+
+  public void gritar(){
+    System.out.println(nome+" esta gritando...");
+  }
+
+}
